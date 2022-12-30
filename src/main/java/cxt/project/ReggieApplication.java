094988264c219e7cddd223c6ff82917689bb61d8
cxy <498111557@qq.com>
2022-12-30 18:28:48 +0000

    feat: init

---
+++ src/main/java/cxt/project/ReggieApplication.java
@@ -0,0 +1,17 @@
+package cxt.project;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class ReggieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class, args);
+        log.info("init...");
+
+    }
+
+}
---
+++ src/main/java/cxt/project/config/WebMVCConfig.java
@@ -0,0 +1,15 @@
+package cxt.project.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebMVCConfig extends WebMvcConfigurationSupport  {
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+}
